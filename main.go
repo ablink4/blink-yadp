b354a7d1220aaf79e00ab1a79e7df47e689f3f06
ablink4 <aaron.blinka@gmail.com>
2025-04-25 22:13:05 -0500

    perf: optimizes queries using batching and goroutines, can currently achieve 1-1.2 M rows per second insert throughput

--- main.go
+++ main.go
@@ -21,63 +21,79 @@ type SensorData struct {
 func main() {
 	ctx := context.Background()
 
-	conn, err := clickhouse.Open(&clickhouse.Options{
-		Addr: []string{"localhost:9000"},
-		Auth: clickhouse.Auth{
-			Database: "default",
-			Username: "default",
-			Password: "aaron",
-		},
-		Protocol: clickhouse.Native,
-	})
-
-	if err != nil {
-		panic(err)
-	}
-
-	if err := conn.Ping(ctx); err != nil {
-		panic(err)
-	}
-	fmt.Println("Connected to ClickHouse.")
-
-	err = conn.Exec(ctx, `
-	CREATE TABLE IF NOT EXISTS sensor_data (
-	id UUID,
-	timestamp DateTime,
-	sensor_id UInt32,
-	value Float32,
-	metadata String
-	) ENGINE = MergeTree()
-	ORDER BY (timestamp)
-	`)
-	if err != nil {
-		panic(err)
-	}
-
 	rand.Seed(int64(time.Now().UnixNano()))
 
-	for {
+	const workers = 4
+
+	for w := 0; w < workers; w++ {
+		go func() {
+			// use a unique conn per goroutine to improve performance
+			conn, err := clickhouse.Open(&clickhouse.Options{
+				Addr: []string{"localhost:9000"},
+				Auth: clickhouse.Auth{
+					Database: "default",
+					Username: "default",
+					Password: "aaron",
+				},
+				Protocol: clickhouse.Native,
+			})
+
+			if err != nil {
+				panic(err)
+			}
+
+			if err := conn.Ping(ctx); err != nil {
+				panic(err)
+			}
+			fmt.Println("Connected to ClickHouse.")
+
+			err = conn.Exec(ctx, `
+			CREATE TABLE IF NOT EXISTS sensor_data (
+			id UUID,
+			timestamp DateTime,
+			sensor_id UInt32,
+			value Float32,
+			metadata String
+			) ENGINE = MergeTree()
+			ORDER BY (timestamp)
+			`)
+			if err != nil {
+				panic(err)
+			}
+
+			// insert data forever
+			for {
+				insertBatch(conn, ctx)
+			}
+		}()
+	}
+
+	select {} // let the goroutines run forever
+}
+
+func insertBatch(conn clickhouse.Conn, ctx context.Context) {
+	batch, err := conn.PrepareBatch(ctx, "INSERT INTO sensor_data")
+	if err != nil {
+		panic(err)
+	}
+
+	batchSize := 100000
+
+	for range batchSize {
 		data := SensorData{
 			uuid.New(),
 			time.Now(),
-			123456, // just have data from one sensor for now
+			rand.Uint32() % 10000, // generate random sensor IDs to mimic multiple sensors
 			rand.Float32() * 10000,
 			"", // no metadata for now
-		}
-
-		batch, err := conn.PrepareBatch(ctx, "INSERT INTO sensor_data")
-		if err != nil {
-			panic(err)
 		}
 
 		if err := batch.AppendStruct(&data); err != nil {
 			panic(err)
 		}
+	}
 
-		if err := batch.Send(); err != nil {
-			panic(err)
-		}
-
-		time.Sleep(1)
+	if err := batch.Send(); err != nil {
+		panic(err)
 	}
 }
