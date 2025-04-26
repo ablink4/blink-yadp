ff86b8df22ed9771bc4f09bc974b18e99078a61a
ablink4 <aaron.blinka@gmail.com>
2025-04-25 22:09:06 -0500

    feat: inserts sensor data cyclically, naive implementation

--- main.go
+++ main.go
@@ -2,22 +2,20 @@ package main
 
 import (
 	"context"
-	"encoding/csv"
 	"fmt"
-	"os"
-	"strconv"
+	"math/rand"
 	"time"
 
 	"github.com/ClickHouse/clickhouse-go/v2"
 	"github.com/google/uuid"
 )
 
-type Event struct {
-	ID        uuid.UUID
-	Timestamp time.Time
-	UserID    uint32
-	EventType string
-	Metadata  string
+type SensorData struct {
+	ID        uuid.UUID `ch:"id"`
+	Timestamp time.Time `ch:"timestamp"`
+	SensorId  uint32    `ch:"sensor_id"`
+	Value     float32   `ch:"value"`
+	Metadata  string    `ch:"metadata"`
 }
 
 func main() {
@@ -43,11 +41,11 @@ func main() {
 	fmt.Println("Connected to ClickHouse.")
 
 	err = conn.Exec(ctx, `
-	CREATE TABLE IF NOT EXISTS events (
+	CREATE TABLE IF NOT EXISTS sensor_data (
 	id UUID,
 	timestamp DateTime,
-	user_id UInt32,
-	event_type String,
+	sensor_id UInt32,
+	value Float32,
 	metadata String
 	) ENGINE = MergeTree()
 	ORDER BY (timestamp)
@@ -56,68 +54,30 @@ func main() {
 		panic(err)
 	}
 
-	events, err := readCSV("data.csv")
-	if err != nil {
-		panic(err)
-	}
+	rand.Seed(int64(time.Now().UnixNano()))
 
-	batch, err := conn.PrepareBatch(ctx, "INSERT INTO events")
-	if err != nil {
-		panic(err)
-	}
+	for {
+		data := SensorData{
+			uuid.New(),
+			time.Now(),
+			123456, // just have data from one sensor for now
+			rand.Float32() * 10000,
+			"", // no metadata for now
+		}
 
-	for _, e := range events {
-		if err := batch.Append(
-			e.ID, e.Timestamp, e.UserID, e.EventType, e.Metadata,
-		); err != nil {
+		batch, err := conn.PrepareBatch(ctx, "INSERT INTO sensor_data")
+		if err != nil {
 			panic(err)
 		}
-	}
 
-	if err := batch.Send(); err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("Insert %d events.\n", len(events))
-}
-
-func readCSV(filename string) ([]Event, error) {
-	f, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close() // defer means this will happen at the end of the function regardless to clean up resources
-
-	reader := csv.NewReader(f)
-	records, err := reader.ReadAll()
-	if err != nil {
-		return nil, err
-	}
-
-	var events []Event
-	for i, row := range records {
-		if i == 0 {
-			continue // skip header
+		if err := batch.AppendStruct(&data); err != nil {
+			panic(err)
 		}
 
-		id, _ := uuid.Parse(row[0])
-		timestamp, err := time.Parse(time.RFC3339, row[1])
-
-		if err != nil {
-			fmt.Printf("Error parsing timestamp '%s': %v\n", row[1], err)
+		if err := batch.Send(); err != nil {
+			panic(err)
 		}
 
-		userID, _ := strconv.ParseUint(row[2], 10, 32)
-
-		event := Event{
-			ID:        id,
-			Timestamp: timestamp,
-			UserID:    uint32(userID),
-			EventType: row[3],
-			Metadata:  row[4],
-		}
-		events = append(events, event)
+		time.Sleep(1)
 	}
-
-	return events, nil
 }
