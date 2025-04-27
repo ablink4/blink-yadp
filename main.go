1ee2876a863b5626a2539074429086f333b8f259
ablink4 <aaron.blinka@gmail.com>
2025-04-26 22:41:37 -0500

    feat: support for millisecond timestamp precision, increased number of goroutines after increasing test VM resources to support it, and added retries with small backoff when server gets overwhelmed.  Insert throughput can get up to 2 million rows per second.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"math/rand"
 	"time"
 
@@ -23,7 +24,7 @@ func main() {
 
 	rand.Seed(int64(time.Now().UnixNano()))
 
-	const workers = 2
+	const workers = 8 // need to tailor this to the resources of the server, otherwise you get CPU or RAM limitation errors
 
 	for w := 0; w < workers; w++ {
 		go func() {
@@ -50,7 +51,7 @@ func main() {
 			err = conn.Exec(ctx, `
 			CREATE TABLE IF NOT EXISTS sensor_data (
 			id UUID,
-			timestamp DateTime,
+			timestamp DateTime64,  
 			sensor_id UInt32,
 			value Float32,
 			metadata String
@@ -75,7 +76,11 @@ func main() {
 func insertBatch(conn clickhouse.Conn, ctx context.Context) {
 	batch, err := conn.PrepareBatch(ctx, "INSERT INTO sensor_data")
 	if err != nil {
-		panic(err)
+		// if the server is overwhelmed you might have an error here, log it and pause for a short time, but don't crash
+		log.Printf("Pausing briefly and then retrying, error is: %s", err)
+		time.Sleep(10 * time.Millisecond) // very brief pause, don't want to wait too long to avoid performance impact
+
+		return // don't continue since the batch isn't valid because of the error
 	}
 
 	batchSize := 100000
